a4ec67b559bd86c9a3fd57b8704c84f480667084
MrRobot175 <m2momen17@gmail.com>
2026-03-23 00:46:43 +0200

    added responsive media queries for mobile and tablet

--- src/Main/App.js
+++ src/Main/App.js
@@ -8,7 +8,7 @@ import Products from "./Products/Products";
 import About from "./About/About";
 function App() {
   return (
-    <BrowserRouter basename="/beta">
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
